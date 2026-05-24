6592d13a80322d94e8932bc4d5dac90205fb316b
tungnt <tungnt.rec@gmail.com>
2026-05-24 13:00:31 +0700

    Claude-Code-Paldark

--- Paldark/Source/Paldark/Sandbox/Lesson01_GameplayTags/SandboxLogCategories.h
+++
@@ -1,7 +0,0 @@
-// SANDBOX: Trong thực tế = PaldarkLogCategories.h với 7 categories
-// (LogPaldark, LogPaldarkPal, LogPaldarkInventory, LogPaldarkNet, LogPaldarkGAS, LogPaldarkBackend, LogPaldarkUI)
-#pragma once
-#include "Logging/LogMacros.h"
-
-DECLARE_LOG_CATEGORY_EXTERN(LogSandboxGeneral, Log, All);
-DECLARE_LOG_CATEGORY_EXTERN(LogSandboxCombat, Log, All);
